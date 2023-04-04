980ca109e1ffe497991b37c4fae0f739cc5848db
joseiltonjunior <joseiltonjuniortech@gmail.com>
2023-04-04 20:52:36 -0300

    ajustando search input

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
-import searchIcon from '@/assets/usopp.png'
+// import searchIcon from '@/assets/usopp.png'
 import xIcon from '@/assets/x-circle.svg'
 
-import { Container } from './styles'
+import { Container, Input, List } from './styles'
 
 import { useState } from 'react'
 
@@ -14,6 +14,7 @@ import { useRouter } from 'next/router'
 
 export function SearchInput() {
   const [listProducts, setListProducts] = useState<ProductInfoProps[]>()
+  const [showList, setShowList] = useState(false)
 
   const dispatch = useDispatch()
 
@@ -35,60 +36,64 @@ export function SearchInput() {
     )
 
     setListProducts(filter)
+    setShowList(true)
   }
 
   return (
-    <Container>
-      <input
-        name="search"
-        type="text"
-        placeholder="Busque aqui"
-        value={valueFilter}
-        autoComplete="off"
-        onFocus={() => handleSearchProduct(valueFilter)}
-        onChange={(e) => {
-          dispatch(filterProducts(e.currentTarget.value))
-          handleSearchProduct(e.currentTarget.value)
-        }}
-      />
-
-      {valueFilter.length > 0 && (
-        <button
-          title="Limpar"
-          onClick={() => {
-            dispatch(filterProducts(''))
-            setListProducts([])
+    <Container onMouseLeave={() => setShowList(false)}>
+      <Input listIsVisible={showList}>
+        <input
+          name="search"
+          type="text"
+          placeholder="Busque aqui"
+          value={valueFilter}
+          autoComplete="off"
+          onClick={() => setShowList(true)}
+          onChange={(e) => {
+            dispatch(filterProducts(e.currentTarget.value))
+            handleSearchProduct(e.currentTarget.value)
           }}
-          style={{
-            marginRight: 10,
+        />
 
-            border: 'none',
-            background: 'transparent',
-            fontSize: '1rem',
-            cursor: 'pointer',
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          <Image src={xIcon} alt="" width={20} height={20} />
-        </button>
-      )}
-
-      <Image src={searchIcon} alt="" width={60} />
-
-      <div className="result">
-        {listProducts?.map((product) => (
+        {valueFilter.length > 0 && (
           <button
-            key={product.id}
+            title="Limpar"
             onClick={() => {
-              router.push(`/product?id=${product.id}`)
+              dispatch(filterProducts(''))
+              setListProducts([])
+              setShowList(false)
+            }}
+            style={{
+              marginRight: 10,
+
+              border: 'none',
+              background: 'transparent',
+              fontSize: '1rem',
+              cursor: 'pointer',
+              display: 'flex',
+              alignItems: 'center',
             }}
           >
-            <Image src={product.imageUrl} alt="" width={30} height={30} />
-            <p>{product.name}</p>
+            <Image src={xIcon} alt="" width={20} height={20} />
           </button>
-        ))}
-      </div>
+        )}
+      </Input>
+
+      <List>
+        {showList &&
+          listProducts?.map((product) => (
+            <button
+              key={product.id}
+              onClick={() => {
+                setShowList(false)
+                router.push(`/product?id=${product.id}`)
+              }}
+            >
+              <Image src={product.imageUrl} alt="" width={30} height={30} />
+              <p>{product.name}</p>
+            </button>
+          ))}
+      </List>
     </Container>
   )
 }
--- src/components/SearchInput/styles.ts
+++ src/components/SearchInput/styles.ts
@@ -1,17 +1,14 @@
 import { styled } from '@/styles'
 
-export const Container = styled('div', {
+export const Input = styled('div', {
   display: 'flex',
   background: '$white',
   alignItems: 'center',
   width: '100%',
 
   borderRadius: 5,
+  overflow: 'hidden',
   height: '35px',
-
-  '@media (min-width: 900px)': {
-    position: 'relative',
-  },
 
   img: {
     objectFit: 'cover',
@@ -21,7 +18,6 @@ export const Container = styled('div', {
   input: {
     height: '100%',
 
-    borderRadius: 5,
     width: '100%',
     border: 'none',
     outline: 'none',
@@ -34,41 +30,63 @@ export const Container = styled('div', {
     },
   },
 
-  'div.result': {
-    position: 'absolute',
-    background: '$white',
-    color: '$gray900',
+  variants: {
+    listIsVisible: {
+      true: {
+        borderBottom: '1px solid $gray100',
 
-    top: 40,
-    left: 0,
-    width: '100%',
-
-    zIndex: 1000,
-
-    '@media (max-width: 900px)': {
-      top: 60,
-    },
-
-    button: {
-      padding: 5,
-      display: 'flex',
-      alignItems: 'center',
-      background: 'transparent',
-      border: 'none',
-      width: '100%',
-      // textDecoration: 'none',
-      color: '$gray900',
-      gap: '1rem',
-      fontSize: '$sm',
-      fontWeight: 'normal',
-
-      '&:hover': {
-        background: '$gray100',
+        '@media (min-width: 900px)': {
+          borderRadius: '5px 5px 0 0',
+        },
       },
-    },
-
-    'button+button': {
-      borderTop: '1px solid $gray100',
     },
   },
 })
+
+export const List = styled('div', {
+  position: 'fixed',
+  background: '$white',
+  color: '$gray900',
+  borderRadius: '0 0 5px 5px',
+  width: '100%',
+  maxWidth: 500,
+
+  zIndex: 1000,
+
+  '@media (max-width: 900px)': {
+    top: 60,
+    left: 0,
+  },
+
+  button: {
+    padding: 5,
+    display: 'flex',
+    alignItems: 'center',
+    background: 'transparent',
+    border: 'none',
+    width: '100%',
+    textAlign: 'left',
+    color: '$gray900',
+    gap: '1rem',
+    fontSize: '$sm',
+    fontWeight: 'normal',
+
+    p: {
+      overflow: 'hidden',
+
+      display: '-webkit-box',
+      '-webkit-line-clamp': '1',
+      '-webkit-box-orient': 'vertical',
+    },
+
+    '&:hover': {
+      background: '$gray100',
+    },
+  },
+
+  'button+button': {
+    borderTop: '1px solid $gray100',
+  },
+})
+
+export const Container = styled('div', {})
