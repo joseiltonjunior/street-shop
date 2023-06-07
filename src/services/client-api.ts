ccabe0604d74d2390221aeea6f0509f3dc6413a9
joseiltonjunior <joseiltonjuniortech@gmail.com>
2023-06-06 22:27:39 -0300

    fix

--- src/services/client-api.ts
+++ src/services/client-api.ts
@@ -1,10 +1,8 @@
 import axios from 'axios'
 
-// console.log(process.env.NEXT_PUBLIC_API_URL)
-
 const clientAPI = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_API_URL,
-  baseURL: 'http://localhost:3333',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  // baseURL: 'http://localhost:3333',
 })
 
 export default clientAPI
